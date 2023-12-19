e6a47a5c517f117524b4794532cb90b4bdf26166
DanilAiro <danchik.b@mail.ru>
2023-12-19 23:37:04 +0300

    v.1.0

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿/*
+  Дан двумерный массив.
+  732
+  496
+  185
+
+  Отсортировать данные в нем по возрастанию.
+ */
+
+int[,] array = { { 7, 3, 2 }, { 4, 9, 6 }, { 1, 8, 5 } };
+PrintTwoDimArray(array);
+Console.WriteLine();
+SortTwoDimArray(array);
+PrintTwoDimArray(array);
+
+void SortTwoDimArray(int[,] arr)
+{
+  int[] temp = FillArrayFromTwoDimArray(arr);
+  Array.Sort(temp);
+  int k = 0;
+
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      arr[i, j] = temp[k++];
+    }
+  }
+}
+
+int[] FillArrayFromTwoDimArray(int[,] arr)
+{
+  int[] temp = new int[arr.GetLength(0) * arr.GetLength(1)];
+  int k = 0;
+
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      temp[k++] = arr[i, j];
+    }
+  }
+
+  return temp;
+}
+
+void PrintTwoDimArray(int[,] arr)
+{
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      Console.Write(arr[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
